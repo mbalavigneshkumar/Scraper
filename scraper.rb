1c3937365d8ff7f0ae81c1a7155b208ff4275676
Balavigneshkumar <m.balavigneshkumar@yahoo.com>
2015-02-20 03:32:47 -0500

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,20 @@
-# This is a template for a Ruby scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/usr/bin/env ruby
+# Saving data:
+# unique_keys = [ 'id' ]
+# data = { 'id'=>12, 'name'=>'violet', 'age'=> 7 }
+# ScraperWiki.save_sqlite(unique_keys, data)
+require 'scraperwiki'
+require 'mechanize'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+for i in 0..150
+ agent = Mechanize.new
+ page = agent.get('https://www.surveymonkey.com/s.aspx?sm=zjAblj+yq7LOd2auA8YfZw==')
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+ search_results = page.form_with(:name => 'surveyForm') do |form|
+  ##form.radiobutton_with(:value => '8601846494').check
+  form.radiobutton_with(:value => '8636393146').check
+ end
+ first_page = agent.submit(search_results)
+ ##puts "Voted #{i}"
+ ##pp first_page
+end
